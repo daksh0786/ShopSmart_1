429a35c0538c1c3b00e6b8ad73859ba74429dc28
Daksh <dakshkhandelwal786@gmail.com>
2024-03-20 04:01:28 +0530

    Intial Setup

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,15 @@
+import Hero from "../Components/Hero/Hero";
+import Offer from "../Components/Offers/Offer";
+import Popular from "../Components/Popular/Popular";
+
+const Shop = () =>{
+    return(
+        <div>
+            <Hero/>
+            <Popular />
+            <Offer />
+        </div>
+    )
+}
+
+export default Shop;
